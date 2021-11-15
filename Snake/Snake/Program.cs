f8c1ce44059b1ca590266178d147fcf0461b6f4b
tzimiscetrane <skul-maks@yandex.ru>
2021-11-15 21:48:25 +0700

    консольная игра змейка

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.SetWindowSize(80, 25);
+			Console.SetBufferSize(80, 25);
+
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Скулкин Максим", xOffset + 2, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+	}
+}
